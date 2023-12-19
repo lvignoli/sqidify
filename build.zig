e9a714ec41418f537843c91eb357a55e95415d22
Louis Vignoli <louis.vignoli@gmail.com>
2023-12-19 19:10:23 +0100

    Add code

---
+++ build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Get the sqids module from the sqids package defined in build.zig.zon.
+    const sqids_dep = b.dependency("sqids", .{});
+    const sqids_module = sqids_dep.module("sqids");
+    //
+
+    const exe = b.addExecutable(.{
+        .name = "encodang",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Make the sqids module available to our executable under the import name "sqids".
+    exe.addModule("sqids", sqids_module);
+    //
+
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,47 @@
+//! Demo program of using Sqids ID.
+//! Usage:
+//!     sqidify a b c …
+//! where a, b, c, … are u64 numbers.
+const std = @import("std");
+const mem = std.mem;
+const sqids = @import("sqids");
+const Sqids = sqids.Sqids;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // Get standard input and output
+    const stdin = std.io.getStdIn().reader();
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    // Read a single line out the input, up to 1000 characters.
+    const max_size = 1000;
+    const buffer: []const u8 = try stdin.readUntilDelimiterAlloc(allocator, '\n', max_size);
+    defer allocator.free(buffer);
+
+    // Parse each space-separated string as a u64 number.
+    var it = mem.splitScalar(u8, buffer, ' ');
+    var arr = std.ArrayList(u64).init(allocator);
+    defer arr.deinit();
+    while (it.next()) |tok| {
+        const n = try std.fmt.parseInt(u64, tok, 10);
+        try arr.append(n);
+    }
+    const numbers = try arr.toOwnedSlice();
+    defer allocator.free(numbers);
+
+    // Using the default Sqids, encode the numbers to a Sqids ID.
+    const s = try Sqids.init(allocator, .{});
+    defer s.deinit();
+    const id = try s.encode(numbers);
+    defer allocator.free(id);
+
+    // Output.
+    try stdout.print("{s}\n", .{id});
+
+    // Flush
+    try bw.flush();
+}
